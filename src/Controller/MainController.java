15a47e00e8905a8324e360afa61b9db6032051c0
MangKyu <whalsrb1226@gmail.com>
2018-12-05 22:56:58 +0900

    Get Current Date

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,20 +1,39 @@
 package Controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainController {
     private Date date;
-    private DatabaseController dc;
-    private UnitController uc;
-    private PlayerController pc;
+    private DatabaseController databaseController;
+    private UnitController unitController;
+    private PlayerController playerController;
 
     public MainController(){
         this.date = new Date();
-        this.dc = new DatabaseController();
-        this.uc = new UnitController();
-        this.pc = new PlayerController();
-
+        this.databaseController= new DatabaseController();
+        this.unitController = new UnitController();
+        this.playerController = new PlayerController();
     }
 
+    public String getCurrentDate(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd a hh:mm:ss ");
+        return dateFormat.format(date);
+    }
 
+    public Date getDate() {
+        return date;
+    }
+
+    public DatabaseController getDatabaseController() {
+        return databaseController;
+    }
+
+    public UnitController getUnitController() {
+        return unitController;
+    }
+
+    public PlayerController getPlayerController() {
+        return playerController;
+    }
 }
