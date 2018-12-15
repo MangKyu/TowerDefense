dde36f2c0830dfbe77c41aaa0273ec963e3c8dc1
swp1234 <swp5516@naver.com>
2018-12-15 15:11:36 +0900

    Unit Creation modifying

--- src/Model/Unit/UnitB.java
+++ src/Model/Unit/UnitB.java
@@ -2,9 +2,17 @@ package Model.Unit;
 
 public class UnitB extends BaseUnit{
 
+    public UnitB(){
+        super();
+    }
+
     @Override
     public void attack() {
         //super.attack();
         System.out.println("UnitA Attack");
+    }
+
+    public void run(){
+
     }
 }
